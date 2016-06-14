d734dc24c26ecd03559cd47bb05a4eaf9184ea5d
akitsu-sanae <akitsu.sanae@gmail.com>
2016-06-14 21:11:40 +0900

    change LICENSE: MIT -> Boost Software License

--- mikayuu/layer.hpp
+++ mikayuu/layer.hpp
@@ -1,3 +1,10 @@
+/*============================================================================
+  Copyright (C) 2016 akitsu sanae
+  https://github.com/akitsu-sanae/mikayuu
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
 #ifndef MIKAYUU_LAYER_HPP
 #define MIKAYUU_LAYER_HPP
 
