f07cf980b25d0d5a8bc5efe6fe1906d2bed5fed6
akitsu-sanae <akitsu.sanae@gmail.com>
2016-10-20 14:22:15 +0900

    fix mkyu.color

--- mikayuu/color.hpp
+++ mikayuu/color.hpp
@@ -10,43 +10,19 @@
 
 namespace mkyu {
 
-template<typename T>
-struct Color final {
-    T r, g, b;
+struct color final {
+    unsigned char r, g, b;
 
-    static Color red();
-    static Color green();
-    static Color blue();
+    static color red() {
+        return color{ 255, 0, 0 };
+    }
+    static color green() {
+        return color{ 0, 255, 0 };
+    }
+    static color blue() {
+        return color{ 0, 0, 255 };
+    }
 };
-
-template<>
-static inline Color<double> Color<double>::red() {
-    return Color<double>{ 1.0, 0.0, 0.0 };
-}
-template<>
-static inline Color<double> Color<double>::green() {
-    return Color<double>{ 0.0, 1.0, 0.0 };
-}
-template<>
-static inline Color<double> Color<double>::blue() {
-    return Color<double>{ 0.0, 0.0, 1.0 };
-}
-template<>
-static inline Color<unsigned char> Color<unsigned char>::red() {
-    return Color<unsigned char>{ 255, 0, 0};
-}
-template<>
-static inline Color<unsigned char> Color<unsigned char>::green() {
-    return Color<unsigned char>{ 0, 255, 0 };
-}
-template<>
-static inline Color<unsigned char> Color<unsigned char>::blue() {
-    return Color<unsigned char>{ 0, 0, 255 };
-}
-
-using Colord = Color<double>;
-using Colori = Color<unsigned char>;
-
 
 }
 
