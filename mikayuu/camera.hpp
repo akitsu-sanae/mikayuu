2d0af3fb8a73c3667111e4336ad371ce8712838b
akitsu-sanae <akitsu.sanae@gmail.com>
2016-10-20 11:52:44 +0900

    fix camera

--- mikayuu/camera.hpp
+++ mikayuu/camera.hpp
@@ -8,8 +8,6 @@ file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
 #ifndef MIKAYUU_CAMERA_HPP
 #define MIKAYUU_CAMERA_HPP
 
-#include <cmath>
-
 #define GLFW_INCLUDE_GLU
 #include <GLFW/glfw3.h>
 
@@ -19,33 +17,16 @@ namespace mkyu {
 
 struct camera {
     void look_at() const {
-        using std::sin;
-        using std::cos;
-        double origin_x = m_origin.radius * sin(m_origin.theta) * cos(m_origin.phi);
-        double origin_y = m_origin.radius * sin(m_origin.theta) * sin(m_origin.phi);
-        double origin_z = m_origin.radius * cos(m_origin.theta);
         gluLookAt(
-            origin_x, origin_y, origin_z,
-            m_target_position[0], m_target_position[1], m_target_position[2],
-            m_up_direction[0], m_up_direction[1], m_up_direction[2]
+            origin_position.x, origin_position.y, origin_position.z,
+            target_position.x, target_position.y, target_position.z,
+            up_direction.x, up_direction.y, up_direction.z
         );
     }
 
-    void move_angle(double dx, double dy);
-    void move_target(double dx, double dy);
-
-    void set_angle(double theta, double phi);
-    void set_target(mkyu::vector3d const&);
-private:
-    struct origin {
-        double theta = 1.0;
-        double phi = 1.0;
-        double radius = 5.0;
-    };
-
-    origin m_origin;
-    mkyu::vector3d m_target_position = mkyu::vector3d{0.0, 0.0, 0.0};
-    mkyu::vector3d m_up_direction = mkyu::vector3d{0.0, 1.0, 0.0};
+    mkyu::vector3d origin_position = mkyu::vector3d{ 5.0, 5.0, 5.0 };
+    mkyu::vector3d target_position = mkyu::vector3d{ 0.0, 0.0, 0.0 };
+    mkyu::vector3d up_direction = mkyu::vector3d{ 0.0, 1.0, 0.0 };
 };
 
 }
